f92efdab1e101431522f47f11bed5b5ee4996c11
Roker2 <dimaminko2000@gmail.com>
2023-12-15 17:15:09 +0100

    GarageScene: Add doors

--- Source/GarageScene.cpp
+++ Source/GarageScene.cpp
@@ -1,5 +1,7 @@
 #include "GarageScene.h"
 
+#include "Controls/DoorButton.h"
+
 USING_NS_AX;
 
 static void problemLoading(const char* filename)
@@ -32,6 +34,15 @@ bool GarageScene::init()
         this->addChild(backgroundSprite, 0);
     }
 
+    auto yellowDoor = createDoor("thirdPartyContent/garage/YellowDoorClosed.png", "thirdPartyContent/garage/YellowDoorOpened.png", Vec2(474.f, 468.f));
+    this->addChild(yellowDoor);
+
+    auto gates = createDoor("thirdPartyContent/garage/GatesClosed.png", "thirdPartyContent/garage/GatesOpened.png", Vec2(-4.f, 458.f));
+    this->addChild(gates);
+
+    auto greenDoor = createDoor("thirdPartyContent/garage/GreenDoorClosed.png", "thirdPartyContent/garage/GreenDoorOpened.png", Vec2(70.f, 455.f));
+    this->addChild(greenDoor);
+
     // scheduleUpdate() is required to ensure update(float) is called on every loop
     scheduleUpdate();
 
@@ -41,4 +52,13 @@ bool GarageScene::init()
 void GarageScene::update(float delta)
 {
 
+}
+
+DoorButton* GarageScene::createDoor(std::string_view normalImage,
+                       std::string_view hoverImage,
+                       const ax::Vec2& pos)
+{
+    auto doorBtn = DoorButton::create(normalImage, hoverImage);
+    doorBtn->setPosition(Vec2(pos.x + doorBtn->getContentSize().x / 2, pos.y - doorBtn->getContentSize().y / 2));
+    return doorBtn;
 }
--- Source/GarageScene.h
+++ Source/GarageScene.h
@@ -2,9 +2,16 @@
 
 #include "axmol.h"
 
+class DoorButton;
+
 class GarageScene : public ax::Scene
 {
 public:
     bool init() override;
     void update(float delta) override;
+
+private:
+    DoorButton* createDoor(std::string_view normalImage = "",
+                           std::string_view hoverImage  = "",
+                           const ax::Vec2& pos          = ax::Vec2());
 };
