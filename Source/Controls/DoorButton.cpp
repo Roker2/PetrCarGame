9bda4a7383f63fa6109d930149add86aab3f216a
Roker2 <dimaminko2000@gmail.com>
2023-12-15 17:14:32 +0100

    Controls: DoorButton: Fix compilation

--- Source/Controls/DoorButton.cpp
+++ Source/Controls/DoorButton.cpp
@@ -1,5 +1,7 @@
 #include "DoorButton.h"
 
+#include "ui/UIScale9Sprite.h"
+
 USING_NS_AX;
 
 DoorButton::DoorButton()
@@ -9,9 +11,9 @@ DoorButton::DoorButton()
 
 }
 
-DoorButton* DoorButton::create(std::string_view normalImage = "",
-                               std::string_view hoverImage  = "",
-                               TextureResType texType       = TextureResType::LOCAL)
+DoorButton* DoorButton::create(std::string_view normalImage,
+                               std::string_view hoverImage,
+                               TextureResType texType)
 {
     DoorButton* btn = new DoorButton;
     if (btn->init(normalImage, hoverImage, texType))
@@ -23,9 +25,9 @@ DoorButton* DoorButton::create(std::string_view normalImage = "",
     return nullptr;
 }
 
-bool DoorButton::init(std::string_view normalImage = "",
-                      std::string_view hoverImage = "",
-                      TextureResType texType = TextureResType::LOCAL)
+bool DoorButton::init(std::string_view normalImage,
+                      std::string_view hoverImage,
+                      TextureResType texType)
 {
     _hoverFileName = hoverImage;
     return Button::init(normalImage, normalImage, normalImage, texType);
