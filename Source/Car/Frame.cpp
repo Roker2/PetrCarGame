e75fc8fbfcbf593af47a2295adad4784261235a1
Roker2 <dimaminko2000@gmail.com>
2023-12-18 19:58:25 +0100

    Car: Frame: Add part areas, onPartTouchMoved method and init method

--- Source/Car/Frame.cpp
+++ Source/Car/Frame.cpp
@@ -1,5 +1,8 @@
 #include "Frame.h"
 
+#include "Part.h"
+#include "PartArea.h"
+
 USING_NS_AX;
 
 namespace car
@@ -21,6 +24,24 @@ Frame* Frame::create(std::string_view filename,
     }
     AX_SAFE_DELETE(frame);
     return nullptr;
+}
+
+bool Frame::initWithFile(std::string_view filename, PixelFormat format)
+{
+    if (!Sprite::initWithFile(filename, format))
+    {
+        return false;
+    }
+
+    _partAreas.push_back(PartArea(256.f, 167.f, 77.f, 35.f, PartType::Engine));
+    return true;
+}
+
+void Frame::onPartTouchMoved(Part* part, Touch* touch, Event* event)
+{
+    if (part->getName().empty())
+        return;
+    AXLOG(part->getName().data());
 }
 
 } // namespace car
--- Source/Car/Frame.h
+++ Source/Car/Frame.h
@@ -5,13 +5,25 @@
 namespace car
 {
 
+class Part;
+class PartArea;
+
 class Frame : public ax::Sprite
 {
+    using PartAreas = std::vector<PartArea>;
+
 public:
     Frame();
 
     static Frame* create(std::string_view filename,
                          ax::PixelFormat format = ax::Texture2D::getDefaultAlphaPixelFormat());
+
+    virtual bool initWithFile(std::string_view filename, ax::PixelFormat format) override;
+
+    void onPartTouchMoved(Part* part, ax::Touch* touch, ax::Event* event);
+
+private:
+    PartAreas _partAreas;
 };
 
 } // namespace car
